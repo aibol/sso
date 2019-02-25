f0d625b63ac7acb920cd4f2d039b0569d45400b4
Shawn Chen <cym@aibol.com>
2019-02-25 09:30:22 +0800

    remove unnecessary projects

--- Initializations/ImportBackstageUsers/SSOModels/AspNetUsers.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace ImportBackstageUsers.SSOModels
-{
-    public class ApplicationUser : IdentityUser<string>
-    {
-        public string Source { get; set; }
-    }
-}
